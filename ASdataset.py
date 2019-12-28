d0178ca7514d89033af3b0843d4a609f5f65016d
v-linhu-gcr231 <v-linhu@microsoft.com>
2019-12-28 06:03:44 +0000

    unet 2down , correct dataset , no month data overlap!!!

--- ASdataset.py
+++ ASdataset.py
@@ -1,5 +1,6 @@
+import gc
 import glob
-
+import bisect
 import numpy as np
 
 import torch
@@ -12,15 +13,27 @@ class AS_Data(Dataset):
         super(AS_Data,self).__init__()
         
         
-
+        self.bucket = [0]
+        self.window = window
         self.EM = []
         self.METCRO2D = []
         self.METCRO3D = []
         self.METCRO3D_5height = []
         self.grid = np.load(glob.glob(cfg['grid'])[0])
         self.label = []
-    
-
+        
+        for filename in sorted(glob.glob(cfg['label'])):
+            print(filename+'   is loading')
+            label = np.load(filename)
+            tick,W,H = label.shape
+            self.label.append(label[int(left*tick):int(right*tick)].copy())
+            self.bucket.append(self.bucket[-1]+int(right*tick)-int(left*tick)-window)
+            del label
+            
+        _,W,H = self.label[0].shape
+        self.W,self.H = W,H
+        
+        
         l_EM = [[0,2,3,4,8,9,10,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50],range(11,30),[6,7],[30,31],[1],[5]]        
         l_METCRO2D = [[0],[3],[5],[11],[12],[13],[14],[17],[18],[19],[20],[26]]
 
@@ -29,58 +42,48 @@ class AS_Data(Dataset):
             EM = np.load(filename)
 #             EM = simplify_matrix(EM,1,l_EM)
             tick,_,W,H = EM.shape
-            self.EM.append(EM[int(left*tick):int(right*tick)])
-        self.EM = np.concatenate(self.EM,axis = 0)
-
-
+            self.EM.append(EM[int(left*tick):int(right*tick)].copy())
+            del EM
+            
         for filename in sorted(glob.glob(cfg['METCRO2D'])):
             print(filename+'   is loading')
             METCRO2D = np.load(filename)
 #             METCRO2D = simplify_matrix(METCRO2D,1,l_METCRO2D)
             tick,_,W,H = METCRO2D.shape
-            self.METCRO2D.append(METCRO2D[int(left*tick):int(right*tick)])
-        self.METCRO2D = np.concatenate(self.METCRO2D,axis = 0)
-
-
+            self.METCRO2D.append(METCRO2D[int(left*tick):int(right*tick)].copy())
+            del METCRO2D
+            
         for filename in sorted(glob.glob(cfg['METCRO3D'])):
             print(filename+'   is loading')
             METCRO3D = np.load(filename)
             tick,_,W,H = METCRO3D.shape
-            self.METCRO3D.append(METCRO3D[int(left*tick):int(right*tick)])
-        self.METCRO3D = np.concatenate(self.METCRO3D,axis = 0)
-
+            self.METCRO3D.append(METCRO3D[int(left*tick):int(right*tick)].copy())
+            del METCRO3D
+            
         for filename in sorted(glob.glob(cfg['METCRO3D_5height'])):
             print(filename+'   is loading')
             METCRO3D_5height = np.load(filename)
             tick,_,W,H = METCRO3D.shape
-            self.METCRO3D_5height.append(METCRO3D_5height[int(left*tick):int(right*tick)])
-        self.METCRO3D_5height = np.concatenate(self.METCRO3D_5height,axis = 0)
+            self.METCRO3D_5height.append(METCRO3D_5height[int(left*tick):int(right*tick)].copy())
+            del METCRO3D_5height
 
-        for filename in sorted(glob.glob(cfg['label'])):
-            print(filename+'   is loading')
-            label = np.load(filename)
-            tick,W,H = label.shape
-            self.label.append(label[int(left*tick):int(right*tick)])
-        self.label = np.concatenate(self.label,axis = 0)
-
-
-        _,W,H = self.label.shape
-        self.W,self.H = W,H
-
-        self.window = window
         
     def __getitem__(self,idx):
+        bucket_idx = bisect.bisect_right(self.bucket,idx)-1
+        idx -= self.bucket[bucket_idx]
         cur = idx+self.window
-        em = self.EM[idx:cur]
-        metcro2d = self.METCRO2D[idx:cur]
-        metcro3d = self.METCRO3D[idx:cur]
-        metcro3d_5height = self.METCRO3D_5height[idx:cur]
-#         grid = np.repeat(self.grid, self.window, axis=0)
-#         grid = grid.reshape((self.window,-1,self.W,self.H))
+
+        em = self.EM[bucket_idx][idx:cur]
+        metcro2d = self.METCRO2D[bucket_idx][idx:cur]
+#         metcro3d = self.METCRO3D[bucket_idx][idx:cur]
+#         metcro3d_5height = self.METCRO3D_5height[bucket_idx][idx:cur]
+        
+        grid = np.repeat(self.grid, self.window, axis=0)
+        grid = grid.reshape((self.window,-1,self.W,self.H))
         
         #metcro3d ,grid 
-        d = np.concatenate([em,metcro2d,metcro3d,metcro3d_5height],axis = 1)
-        return d,self.grid,self.label[cur]
+        d = np.concatenate([em,metcro2d],axis = 1) #metcro3d metcro3d_5height
+        return d,self.grid,self.label[bucket_idx][cur]
         
     def __len__(self):
-        return self.label.shape[0]-self.window
+        return self.bucket[-1]-1
--- model/unet_model.py
+++ model/unet_model.py
@@ -1,6 +1,7 @@
 """ Full assembly of the parts to form the complete network """
 
 import torch.nn.functional as F
+from torchvision.models.resnet import BasicBlock,conv1x1,conv3x3
 
 from .unet_parts import *
 
@@ -15,41 +16,133 @@ class UNet(nn.Module):
             nn.Conv2d(grid_dim, 32, kernel_size=7, stride=1, padding=3,
                                bias=False),
             nn.ReLU(inplace=True),
-            nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1,
+            nn.Conv2d(32, 16, kernel_size=3, stride=1, padding=1,
                                bias=False),
             nn.ReLU(inplace=True),
-            nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1,
+            nn.Conv2d(16, 16, kernel_size=3, stride=1, padding=1,
                                bias=False),
                                )
         
+        
+        self.bn0 = norm_layer(n_channels)
         self.n_channels = n_channels
         self.bilinear = bilinear
-
+        
+#         downsample = nn.Sequential(
+#                 conv1x1(n_channels, planes * 1, stride=1),
+#                 norm_layer(planes * 1),
+#             )
         self.inc = DoubleConv(n_channels, 64)
+#         sel.inc = nn.Sequential(
+#             BasicBlock(n_channels, 64,downsample),
+#             BasicBlock(64, 64)
+#         )
+        
         self.down1 = Down(64, 128)
-        self.down2 = Down(128, 256)
-        self.down3 = Down(256, 256)
+        self.down2 = Down(128, 128)
+#         self.down2 = Down(128, 256)
+#         self.down3 = Down(256, 512)
 #         self.down4 = Down(512, 512)
 #         self.up1 = Up(1024, 256, bilinear)
-        self.up2 = Up(512, 128, bilinear)
+#         self.up2 = Up(512, 128, bilinear)
+#         self.up3 = Up(256, 64, bilinear)
         self.up3 = Up(256, 64, bilinear)
         self.up4 = Up(128, 64, bilinear)
-        self.outc = OutConv(64, 4)
+        self.outc = OutConv(64, 2)
                 
-        self.outt = nn.Sequential(nn.Linear(4*T+32,128),
-                                  nn.ReLU(),
-                                  nn.Linear(128,1))
+        self.outt = nn.Sequential(nn.Linear(T*2+16,64),
+                                nn.ReLU(inplace=True),
+                                nn.Linear(64,1)
+                               )
         
         
     def forward(self, x,grid):
+        x = self.bn0(x)
         x1 = self.inc(x)
         x2 = self.down1(x1)
         x3 = self.down2(x2)
-        x4 = self.down3(x3)
+#         x4 = self.down3(x3)
 #         x5 = self.down4(x4)
 #         x = self.up1(x5, x4)
-        x = self.up2(x4, x3)
-        x = self.up3(x, x2)
+#         x = self.up2(x, x3)
+        x = self.up3(x3, x2)
+        x = self.up4(x, x1)
+        
+        logits = self.outc(x)
+        T,_,W,H = logits.shape
+        logits = logits.permute(2,3,0,1)
+        logits = logits.view(W,H,-1)
+        
+        grid = self.conv_grid(grid)
+        grid = grid.permute(2,3,0,1)
+        grid = grid.view(W,H,-1)
+        
+        logits = torch.cat([logits,grid],dim = -1)
+        logits = self.outt(logits)
+        return logits
+    
+    
+    
+
+class UNet_Res(nn.Module):
+    def __init__(self, n_channels,grid_dim, T, bilinear=True):
+        super(UNet_Res, self).__init__()
+        
+        norm_layer = nn.BatchNorm2d
+        self.conv_grid = nn.Sequential(
+            norm_layer(grid_dim),
+            nn.Conv2d(grid_dim, 32, kernel_size=7, stride=1, padding=3,
+                               bias=False),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(32, 16, kernel_size=3, stride=1, padding=1,
+                               bias=False),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(16, 8, kernel_size=3, stride=1, padding=1,
+                               bias=False),
+                               )
+        
+        self.inplanes = 64
+        self.bn0 = norm_layer(n_channels)
+        self.conv0 = nn.Conv2d(n_channels, self.inplanes, kernel_size=7, stride=1, padding=3,
+                               bias=False)
+        
+        self.bilinear = bilinear
+        
+#         self.inc = DoubleConv(n_channels, 64)
+        self.inc = nn.Sequential(
+            BasicBlock(64, 64),
+#             BasicBlock(64, 64)
+        )
+        
+        self.down1 = Down(64, 128)
+        self.down2 = Down(128, 128)
+#         self.down2 = Down(128, 256)
+#         self.down3 = Down(256, 512)
+#         self.down4 = Down(512, 512)
+#         self.up1 = Up(1024, 256, bilinear)
+#         self.up2 = Up(512, 128, bilinear)
+#         self.up3 = Up(256, 64, bilinear)
+        self.up3 = Up(256, 64, bilinear)
+        self.up4 = Up(128, 64, bilinear)
+        self.outc = OutConv(64, 1)
+                
+        self.outt = nn.Sequential(nn.Linear(T,1))
+#                                   nn.ReLU(),
+#                                   nn.Linear(64,1))
+        
+        
+    def forward(self, x,grid):
+        x = self.bn0(x)
+        x = self.conv0(x)
+        
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+#         x4 = self.down3(x3)
+#         x5 = self.down4(x4)
+#         x = self.up1(x5, x4)
+#         x = self.up2(x, x3)
+        x = self.up3(x3, x2)
         x = self.up4(x, x1)
         
         logits = self.outc(x)
