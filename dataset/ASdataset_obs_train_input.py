700277407a0c3ebc49e3877610e90ece4b4d92ac
v-linhu-gcr231 <v-linhu@microsoft.com>
2020-03-28 02:37:53 +0000

    model train input to fit obs_label

---
+++ dataset/ASdataset_obs_train_input.py
@@ -0,0 +1,111 @@
+import gc
+import glob
+import bisect
+import numpy as np
+
+import torch
+from torch.utils.data import DataLoader,Dataset
+
+from util import simplify_matrix
+
+class AS_Data(Dataset):
+    def __init__(self,cfg,left = 0,right = 1,window=24):
+        super(AS_Data,self).__init__()
+        
+        
+        self.bucket = [0]
+        self.window = window
+        self.EM = []
+        self.METCRO2D = []
+        self.METCRO3D = []
+        self.METCRO3D_5height = []
+        self.grid = np.load(glob.glob(cfg['grid'])[0])
+        self.label = []
+        self.obs_label = []
+        
+        for filename in sorted(glob.glob(cfg['label'])):
+            print(filename+'   is loading')
+            label = np.load(filename)
+            tick,W,H = label.shape
+            self.label.append(label[int(left*tick):int(right*tick)].copy())
+            self.bucket.append(self.bucket[-1]+int(right*tick)-int(left*tick)-window)
+            del label
+        
+        _,W,H = self.label[0].shape
+        self.W,self.H = W,H
+        ####"NO2","SO2","O3","PM2.5","PM10","CO"  need to set
+        self.obs_label_idx = 2 if 'O3' in cfg['label'] else 3
+        for filename in sorted(glob.glob(cfg['obs_label'])):
+            print(filename+'   is loading')
+            obs_label = np.load(filename)
+            tick,_,W,H = obs_label.shape
+            self.obs_label.append(obs_label[int(left*tick):int(right*tick),self.obs_label_idx].copy())
+            del obs_label
+        
+        
+        l_EM = [[0,2,3,4,8,9,10,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50],range(11,30),[6,7],[30,31],[1],[5]]        
+        l_METCRO2D = [[0],[3],[5],[11],[12],[13],[14],[17],[18],[19],[20],[26]]
+
+        for filename in sorted(glob.glob(cfg['METCRO3D_5height'])):
+            print(filename+'   is loading')
+            METCRO3D_5height = np.load(filename)
+            tick,_,W,H = METCRO3D_5height.shape
+            self.METCRO3D_5height.append(METCRO3D_5height[int(left*tick):int(right*tick)].copy())
+            del METCRO3D_5height
+
+            
+        for filename in sorted(glob.glob(cfg['EM'])):
+            print(filename+'   is loading')
+            EM = np.load(filename)
+#             EM = simplify_matrix(EM,1,l_EM)
+            tick,_,W,H = EM.shape
+            self.EM.append(EM[int(left*tick):int(right*tick)].copy())
+            del EM
+            
+        for filename in sorted(glob.glob(cfg['METCRO2D'])):
+            print(filename+'   is loading')
+            METCRO2D = np.load(filename)
+#             METCRO2D = simplify_matrix(METCRO2D,1,l_METCRO2D)
+            tick,_,W,H = METCRO2D.shape
+            self.METCRO2D.append(METCRO2D[int(left*tick):int(right*tick)].copy())
+            del METCRO2D
+            
+        for filename in sorted(glob.glob(cfg['METCRO3D'])):
+            print(filename+'   is loading')
+            METCRO3D = np.load(filename)
+            tick,_,W,H = METCRO3D.shape
+            self.METCRO3D.append(METCRO3D[int(left*tick):int(right*tick)].copy())
+            del METCRO3D
+            
+        
+        
+    def __getitem__(self,idx):
+        bucket_idx = bisect.bisect_right(self.bucket,idx)-1
+        idx -= self.bucket[bucket_idx]
+        cur = idx+self.window
+
+        em = self.EM[bucket_idx][idx:cur]
+        metcro2d = self.METCRO2D[bucket_idx][idx:cur]
+#         metcro3d = self.METCRO3D[bucket_idx][idx:cur]
+#         metcro3d_5height = self.METCRO3D_5height[bucket_idx][idx:cur]
+        
+        grid = np.repeat(self.grid, self.window, axis=0)
+        grid = grid.reshape((self.window,-1,self.W,self.H))
+        
+        #metcro3d ,grid 
+        d = np.concatenate([em,metcro2d],axis = 1) #metcro3d metcro3d_5height
+        
+#         mi = np.min(d,axis = (2,3),keepdims = True)
+#         ma = np.max(d,axis = (2,3),keepdims = True)
+#         m = (mi+ma)/2
+#         lower = d.copy()
+#         higher = d.copy()
+#         m = np.tile(m,[1,1,lower.shape[2],lower.shape[3]])
+#         lower[lower>=m] = m[lower>=m]
+#         higher[higher<=m] = m[higher<=m]
+#         d = np.concatenate([lower,higher],axis = 1)
+        
+        return d,self.grid,self.label[bucket_idx][cur-self.window],self.label[bucket_idx][cur],self.obs_label[bucket_idx][cur]
+        
+    def __len__(self):
+        return self.bucket[-1]-1
