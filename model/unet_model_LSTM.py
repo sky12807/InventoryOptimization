e8957860060e453603294143c1a22d68c00a2ad3
v-zeyyan <v-zeyyan@microsoft.com>
2020-10-23 15:19:02 +0000

    methodA

--- model/unet_model_LSTM.py
+++ model/unet_model_LSTM.py
@@ -27,37 +27,17 @@ class UNet(nn.Module):
         self.bn0 = norm_layer(n_channels)
         self.n_channels = n_channels
         self.bilinear = bilinear
-        
-#         downsample = nn.Sequential(
-#                 conv1x1(n_channels, planes * 1, stride=1),
-#                 norm_layer(planes * 1),
-#             )
 
         self.h0 = nn.Parameter(torch.randn(1,1,64))
         self.c0 = nn.Parameter(torch.randn(1,1,64))
         self.rnn = nn.LSTM(n_channels,64,num_layers=1,batch_first=True)
         
         self.inc = DoubleConv(64, 64)
-#         sel.inc = nn.Sequential(
-#             BasicBlock(n_channels, 64,downsample),
-#             BasicBlock(64, 64)
-#         )
-        
-        
-        
     
         self.down1 = Down(64, 128)
         self.down2 = Down(128, 128)
-#         self.down2 = Down(128, 256)
-#         self.down3 = Down(256, 512)
-#         self.down4 = Down(512, 512)
-#         self.up1 = Up(1024, 256, bilinear)
-#         self.up2 = Up(512, 128, bilinear)
-#         self.up3 = Up(256, 64, bilinear)
         self.up3 = Up(256, 64, bilinear)
         self.up4 = Up(128, 64, bilinear)
-#         self.outc = OutConv(64, 8)
-
         
         self.conv_y_s = nn.ModuleList()
         self.out_conv_s = nn.ModuleList()
@@ -75,7 +55,7 @@ class UNet(nn.Module):
                                                    )
                         )
             
-            self.out_conv_s.append(nn.Sequential(nn.Conv2d(64+8+8, 64, kernel_size=1), #(8+8+8,64),
+            self.out_conv_s.append(nn.Sequential(nn.Conv2d(64+8, 64, kernel_size=1), #(8+8+8,64),
                                                 nn.LeakyReLU(),
                                                 nn.Conv2d(64, 1, kernel_size=1))
                                   )
@@ -87,54 +67,174 @@ class UNet(nn.Module):
         
         #x B*T*C*H*W
         B,T,C,H,W = x.shape
+#         print(f'Initial x shape: {x.shape}')
+#         print(f'Grid shape: {grid.shape}')
+#         print(f'yt shape: {yt_1.shape}')
         x = x.reshape(B*T,C,H,W)
+#         print(f'Reshape x shape: {x.shape}')
         x = self.bn0(x)
+#         print(f'Batch Norm shape: {x.shape}')
         x = x.reshape(B,T,C,H,W)
+#         print(f'Reshape x shape: {x.shape}')
         
         x = x.permute(0,3,4,1,2) #x B*H*W*T*C
+#         print(f'Permute x shape: {x.shape}')
         x = x.reshape(B*H*W,T,C)
+#         print(f'Reshape x shape: {x.shape}')
         h0 = self.h0.repeat(1,B*H*W,1)
         c0 = self.c0.repeat(1,B*H*W,1)
+#         print(f'h0 shape: {h0.shape}')
+#         print(f'c0 shape: {c0.shape}')
         _,(hn,cn) = self.rnn(x,(h0,c0)) #hn num_layers*(B*H*W)*rnn_hidden
-        
+#         print(f'hn shape: {hn.shape}')
+#         print(f'cn shape: {cn.shape}')
         x = hn[-1]
+#         print(f'x shape: {x.shape}')
         x = x.reshape(B,H,W,-1)
+#         print(f'Reshape x shape: {x.shape}')
         x = x.permute(0,3,1,2)
+#         print(f'Permute x shape: {x.shape}')
         x = x.reshape(B,-1,H,W)
+#         print(f'Reshape x shape: {x.shape}')
         
         x1 = self.inc(x)
+#         print(f'After Double Conv: {x1.shape}')
         x2 = self.down1(x1)
+#         print(f'After Down 1: {x2.shape}')
         x3 = self.down2(x2)
-#         x4 = self.down3(x3)
-#         x5 = self.down4(x4)
-#         x = self.up1(x5, x4)
-#         x = self.up2(x, x3)
+#         print(f'After Down 2: {x2.shape}')
         x = self.up3(x3, x2)
+#         print(f'After up 3: {x.shape}')
         x = self.up4(x, x1)
+#         print(f'After up 4: {x.shape}')
         
-#         logits = self.outc(x) #B*T,_,H,W
         logits = x.reshape(B,-1,H,W)
-#         logits = logits.permute(2,3,0,1)
-#         logits = logits.view(W,H,-1)
+#         print(f'Reshape x to logits: {logits.shape}')
         
-        #grid:B*27*H*W
         grid = self.conv_grid(grid)
-#         grid = grid.permute(2,3,0,1)
-#         grid = grid.view(W,H,-1)
-        
+#         print(f'Grid: {grid.shape}')
         
         #yt_1:B*pre_dim*H*W        
         logits = torch.cat([logits,grid],dim = 1)
-        
+#         print(f'Concat logits and grid: {logits.shape}')
         out = []
         for air in range(self.pre_dim):
-            yt_1_now = self.conv_y_s[air](yt_1[:,air:air+1])
-            out.append(self.out_conv_s[air](torch.cat([logits,yt_1_now],dim = 1)))
+#             yt_1_now = self.conv_y_s[air](yt_1[:,air:air+1])
+#             print(f'new yt shape: {yt_1_now.shape}')
+#             out.append(self.out_conv_s[air](torch.cat([logits,yt_1_now],dim = 1)))
+#             print(f'part out shape: {out[-1].shape}')
+            out.append(self.out_conv_s[air](logits))
             
         out = torch.cat(out,dim = 1)
+#         print(f'out shape: {out.shape}')
         return out
     
+class ReverseModel(nn.Module):
+    def __init__(self, n_channels,grid_dim,pre_dim,T,em_dim):
+        super(ReverseModel, self).__init__()        
+        self.bn_metrod = nn.BatchNorm2d(n_channels)
+        self.bn_y = nn.BatchNorm2d(pre_dim)
+        self.bn_grid = nn.BatchNorm2d(grid_dim)
+        self.bn_y_grid = nn.BatchNorm2d(T)
+        self.n_channels = n_channels
+        self.rnn = nn.LSTM(n_channels+pre_dim,64,num_layers=1,batch_first=True)
+        self.linear = nn.Sequential(
+            nn.Linear(64+8,32),
+            nn.LeakyReLU(),
+            nn.Linear(32,em_dim)
+        )
+        
+        self.h0 = nn.Parameter(torch.randn(1,1,64))
+        self.c0 = nn.Parameter(torch.randn(1,1,64))
+        
+        self.inc = DoubleConv(64, 64)
     
+        self.down1 = Down(64, 128)
+        self.down2 = Down(128, 128)
+        self.up3 = Up(256, 64, bilinear)
+        self.up4 = Up(128, 64, bilinear)
+        
+        self.conv_grid = nn.Sequential(
+            norm_layer(grid_dim),
+            nn.Conv2d(grid_dim, 32, kernel_size=7, stride=1, padding=3,
+                               bias=False),
+            nn.LeakyReLU(),
+            nn.Conv2d(32, 16, kernel_size=3, stride=1, padding=1,
+                               bias=False),
+            nn.LeakyReLU(),
+            nn.Conv2d(16, 8, kernel_size=3, stride=1, padding=1,
+                               bias=False),
+                               )
+
+    def forward(self,metrod,grid,all_label):
+#         print(f'Metrod shape: {metrod.shape}')
+#         print(f'Grid shape: {grid.shape}')
+#         print(f'All Label shape: {all_label.shape}')
+        # Batch Normalization for Metrod
+        B, T, C, H, W = metrod.shape
+        metrod = metrod.reshape(B*T,C,H,W)
+        metrod = self.bn_metrod(metrod)
+        metrod = metrod.reshape(B,T,C,H,W)
+#         print(f'Metrod shape: {metrod.shape}')
+        
+        B, T, C, H, W = all_label.shape
+        y = all_label.reshape(B*T,C,H,W)
+        y = self.bn_y(y)
+        y = y.reshape(B,T,C,H,W)
+#         print(f'Y shape: {y.shape}')
+        
+        grid = self.bn_grid(grid)
+        
+        # Concat y and metrod
+        y_metrod = torch.cat([y, metrod],dim = 2)
+#         print(f'y_metrod shape: {y_metrod.shape}')
+        
+        B, T, C, H, W = y_metrod.shape
+        y_metrod = y_metrod.permute(0,3,4,1,2)
+        y_metrod = y_metrod.reshape(B*H*W,T,C)
+#         print(f'reshape y_metrod shape: {y_metrod.shape}')
+        
+        h0 = self.h0.repeat(1,B*H*W,1)
+        c0 = self.c0.repeat(1,B*H*W,1)
+#         print(f'h0 shape: {h0.shape}')
+#         print(f'c0 shape: {c0.shape}')
+        _,(hn,cn) = self.rnn(y_metrod,(h0,c0)) #hn num_layers*(B*H*W)*rnn_hidden
+#         print(f'hn shape: {hn.shape}')
+#         print(f'cn shape: {cn.shape}')
+        y = hn[-1]
+#         print(f'y shape: {y.shape}')
+        y = y.reshape(B,H,W,-1)
+        y = y.permute(0,3,1,2)
+#         print(f'reshape y shape: {y.shape}')
+        
+        y1 = self.inc(y)
+#         print(f'After Double Conv: {y1.shape}')
+        y2 = self.down1(y1)
+#         print(f'After Down 1: {y2.shape}')
+        y3 = self.down2(y2)
+#         print(f'After Down 2: {y3.shape}')
+        y = self.up3(y3, y2)
+#         print(f'After up 3: {y.shape}')
+        y = self.up4(y, y1)
+#         print(f'After up 4: {y.shape}')
+        
+        logits = y.reshape(B,-1,H,W)
+#         print(f'Reshape y to logits: {logits.shape}')
+        
+        grid = self.conv_grid(grid)
+#         print(f'Grid: {grid.shape}')
+        
+        #Concat logits and grid
+        logit_grid = torch.cat([logits,grid],dim=1)
+#         print(f'logit_grid shape: {logit_grid.shape}')
+        
+        out = self.linear(logit_grid)
+#         print(f'output shape: {out.shape}')
+        B,C,H,W = out.shape
+        out = out.reshape(B,1,C,H,W)
+#         print(f'final output shape: {out.shape}')
+        
+        return out
     
 
 class UNet_Res(nn.Module):
@@ -160,21 +260,19 @@ class UNet_Res(nn.Module):
                                bias=False)
         
         self.bilinear = bilinear
-        
-#         self.inc = DoubleConv(n_channels, 64)
         self.inc = nn.Sequential(
             BasicBlock(64, 64),
-#             BasicBlock(64, 64)
+            # BasicBlock(64, 64)
         )
         
         self.down1 = Down(64, 128)
         self.down2 = Down(128, 128)
-#         self.down2 = Down(128, 256)
-#         self.down3 = Down(256, 512)
-#         self.down4 = Down(512, 512)
-#         self.up1 = Up(1024, 256, bilinear)
-#         self.up2 = Up(512, 128, bilinear)
-#         self.up3 = Up(256, 64, bilinear)
+        # self.down2 = Down(128, 256)
+        # self.down3 = Down(256, 512)
+        # self.down4 = Down(512, 512)
+        # self.up1 = Up(1024, 256, bilinear)
+        # self.up2 = Up(512, 128, bilinear)
+        # self.up3 = Up(256, 64, bilinear)
         self.up3 = Up(256, 64, bilinear)
         self.up4 = Up(128, 64, bilinear)
         self.outc = OutConv(64, 1)
@@ -182,9 +280,9 @@ class UNet_Res(nn.Module):
         self.outt = nn.Sequential(OutConv(64, 1))
         
         
-#         nn.Linear(T,1))
-#                                   nn.ReLU(),
-#                                   nn.Linear(64,1))
+        # nn.Linear(T,1))
+        #                           nn.ReLU(),
+        #                           nn.Linear(64,1))
         
         
     def forward(self, x,grid):
@@ -194,10 +292,6 @@ class UNet_Res(nn.Module):
         x1 = self.inc(x)
         x2 = self.down1(x1)
         x3 = self.down2(x2)
-#         x4 = self.down3(x3)
-#         x5 = self.down4(x4)
-#         x = self.up1(x5, x4)
-#         x = self.up2(x, x3)
         x = self.up3(x3, x2)
         x = self.up4(x, x1)
         
--- test_step1.py
+++ test_step1.py
@@ -15,13 +15,34 @@ from torch.utils.data import DataLoader,Dataset
 
 import logging
 from importlib import reload  # Not needed in Python 2
+reload(logging)
 
 torch.backends.cudnn.enabled = False
 torch.backends.cudnn.deterministic = True
 device = torch.device("cuda"  if torch.cuda.is_available() else "cpu")
 
+logging.basicConfig(level=logging.INFO,#控制台打印的日志级别
+                    filename='logging.txt',
+                    filemode='a',##模式，有w和a，w就是写模式，每次都会重新写日志，覆盖之前的日志
+                    #a是追加模式，默认如果不写的话，就是追加模式
+                    format=
+                    '%(asctime)s : %(message)s',
+                    )
+
+
+
 from dataset.ASdataset import AS_Data
 from dataset.ASdataset_obs_train_input import AS_Data_obs
+import json
+
+# NORM_METHOD = 2
+
+# if NORM_METHOD == 1:
+#     with open('Norm1.json', "r") as f:
+#         dic = json.load(f)
+# elif NORM_METHOD == 2:
+#     with open('Norm2.json', "r") as f:
+#         dic = json.load(f)
 
 with open('config/cfg.yaml','r') as f:
     cfg = yaml.load(f)
@@ -44,10 +65,10 @@ print(len(test_Data))
 from model.res_model_LSTM import res8
 from model.unet_model_LSTM import UNet
 
-# test_model = res8(51+34,27,inplanes=64,layers = [2],T=T,pre_dim = 2) #+5*16
-# name = 'res'
-test_model = UNet(cfg['meteorological_dim']+cfg['emission_dim'],cfg['grid_dim'],T=T,bilinear=False,pre_dim = len(pollution)) #+80
+# test_model = res8(cfg['meteorological_dim']+cfg['emission_dim'],cfg['grid_dim'],T=T,pre_dim = len(pollution)) #+5*16
 name = cfg['name']
+test_model = UNet(cfg['meteorological_dim']+cfg['emission_dim'],cfg['grid_dim'],T=T,bilinear=False,pre_dim = len(pollution)) #+80
+# name = cfg['name']
 # test_model.load_state_dict(torch.load('model_save/o3_best_unet2_1month_65_epoch.t'))
 
 test_model.to(device)
@@ -59,10 +80,16 @@ def score(model,loader,criterion= nn.L1Loss(),percent = False):
     ls = []
     for idx,i in enumerate(loader):
         with torch.no_grad():
-            input,grid,yt_1,label = i
-            input,grid,yt_1,label = input.to(device),grid.to(device),yt_1.to(device),label.to(device)
+            input,grid,yt_1,label,all_label= i
+            input,grid,yt_1,label,all_label = input.to(device),grid.to(device),yt_1.to(device),label.to(device),all_label.to(device)
             y_pred = model(input,grid,yt_1)
             
+#             if NORM_METHOD == 1:
+#                 y_pred = y_pred * (dic['max'] - dic['min']) + dic['min']
+#                 label = label * (dic['max'] - dic['min']) + dic['min']
+#             elif NORM_METHOD == 2:
+#                 y_pred = (y_pred + dic['min']) * dic['std'] + dic['mean']
+#                 label = (label + dic['min']) * dic['std'] + dic['mean']
             cur_loss = []
             for j in range(label.shape[1]):
                 if percent:
@@ -80,17 +107,18 @@ def score(model,loader,criterion= nn.L1Loss(),percent = False):
 best_score = 1000
 early_stop = 15
 early_cnt = 0
-for epoch in range(21):
-    logging.info('-----------{}-----------'.format(epoch))
+for epoch in range(25):
+#     logging.info('-----------{}-----------'.format(epoch))
+    print('-----------{}-----------'.format(epoch))
     ls = []
     
     test_model.train()
     for idx,i in enumerate(trainloader):
-        print(idx)
-        input,grid,yt_1,label = i
-        input,grid,yt_1,label = input.to(device),grid.to(device),yt_1.to(device),label.to(device)
+#         print(idx)
+        input,grid,yt_1,label,all_label= i
+        input,grid,yt_1,label,all_label= input.to(device),grid.to(device),yt_1.to(device),label.to(device),all_label.to(device)
         y_pred = test_model(input,grid,yt_1)
-        
+#         print('*'*20)
         assert y_pred.shape == label.shape
         
         optimizer.zero_grad()
@@ -100,21 +128,27 @@ for epoch in range(21):
         optimizer.step()
         ls.append(loss.cpu().data)
         if len(ls)%40==0:
+            logging.info('epoch {} cur loss {}'.format(epoch,np.mean(ls)))
             print('epoch {} cur loss {}'.format(epoch,np.mean(ls)))
+#         torch.save(test_model.cpu().state_dict(),'model_save/test.t')
     
+#     logging.info('epoch {} cur loss {}'.format(epoch,np.mean(ls)))
     print('epoch {} cur loss {}'.format(epoch,np.mean(ls)))
     test_score_L1 = score(test_model,testloader,criterion = nn.L1Loss()) 
+#     logging.info('-------------cur test loss L1:  {}'.format(','.join([str(s) for s in test_score_L1])))
     print('-------------cur test loss L1:  {}'.format(','.join([str(s) for s in test_score_L1])))
     
 #     if epoch%5 == 0:
 #         torch.save(test_model.cpu().state_dict(),'model_save/{}_{}_epoch.t'.format(name,epoch))
 #         test_model.to(device)
+
     if np.sum(test_score_L1)<best_score:
         early_cnt = 0
         best_score = np.sum(test_score_L1)
         torch.save(test_model.cpu().state_dict(),name)
         test_model.to(device)
-    else:
-        early_cnt += 1
-        if early_cnt>=early_stop:
-            break
+        print("This is current best epoch")
+    # else:
+    #     early_cnt += 1
+    #     if early_cnt>=early_stop:
+    #         break
